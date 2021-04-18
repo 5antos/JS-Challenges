698a52093ce37dd87201d218088729caf60b35f0
5antos <a67984@ualg.pt>
2021-04-18 03:32:10 +0100

    Count the total number of occurrences of vowels in string

---
+++ countVowels.js
@@ -0,0 +1,13 @@
+/**
+ * Coding Challenge #8 - Find the Vowels
+ * 
+ * Write a function that takes a string as argument and returns the number of vowels contained in that string.
+ * 
+ * @author 5antos#4876
+ * @param {string} string String to check
+ * @returns {number} Number of vowels contained in the provided string
+ */
+
+function getVowelsCount(string) {
+  return string.replace(/[^aeiou]/g, '').length
+}
