c78f2ffea711d28781dbb29854a0bc4f86d933f7
5antos <a67984@ualg.pt>
2021-04-18 03:33:40 +0100

    Check if number is an Armstrong number

---
+++ isArmstrongNumber.js
@@ -0,0 +1,20 @@
+/**
+ * Coding Challenge #6 - Armstrong Numbers
+ * 
+ * An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+ * Determine if the input number is an Armstrong number.
+ * Return either true or false.
+ * 
+ * *: Generic: aⁿ + bⁿ + cⁿ + dⁿ = abcd, where n = number of digits
+ * *: Example: 1³ + 5³ + 3³ = 153, n = 3
+ * 
+ * @author 5antos#4876
+ * @param {number} num Number to check
+ * @returns {boolean} Whether or not the provided number is an Armstrong number
+ */
+
+function isArmstrongNumber(num) {
+  const str = ''+num
+  const { length } = str
+  return num === str.split('').reduce((acc, curr) => acc + (+curr)**length, 0)
+}
