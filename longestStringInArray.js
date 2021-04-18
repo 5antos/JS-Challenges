cdaf0d783e4a5a015ac7c24f5a3f48b59786d8d2
5antos <a67984@ualg.pt>
2021-04-18 03:34:04 +0100

    Get the longest string in array

---
+++ longestStringInArray.js
@@ -0,0 +1,18 @@
+/**
+ * Coding Challenge #1 - Longest String in Array
+ * 
+ * Write a function that accepts an array of strings.
+ * Return the longest string.
+ * 
+ * @author 5antos#4876
+ * @param {string[]} strings Array of strings
+ * @returns {string} Longest string from the provided array
+ */
+
+function getLongestString(strings) {
+  let longest = '';
+  for (var i = 0; i < strings.length; i++)
+    if (strings[i].length > longest.length)
+      longest = strings[i]
+  return longest
+}
