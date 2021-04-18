76e67a00002315d3a5abb0253440e17ba0f4e598
5antos <a67984@ualg.pt>
2021-04-18 03:46:30 +0100

    Check if string contains a valid nesting of braces

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -1,5 +1,5 @@
 /**
- * Coding Challenge #5 - Balanced Brackets
+ * Balanced Brackets
  * 
  * Given a string possibly containing three types of braces ({}, [], ()),
  * write a function that returns a Boolean indicating whether the given string contains a valid nesting of braces.
